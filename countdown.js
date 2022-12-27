42c680f5c9647d5a13142ed91fa8fa18a96561d9
jonaaldas <jonaaldas@gmail.com>
2022-12-27 18:58:35 -0500

    starter files

---
+++ countdown.js
@@ -0,0 +1,27 @@
+let time = '12-30-2024 16:00';
+let startCountDown = (time) => {
+	let startTimer = () => {
+		let now = new Date().getTime();
+		let countDownDate = new Date(`${time}`).getTime();
+		let distance = countDownDate - now;
+		let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		let hours = Math.floor(
+			(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+		);
+		let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		document.querySelector('.inner-days').innerText = days;
+		document.querySelector('.inner-hour').innerText = hours;
+		document.querySelector('.inner-min').innerText = minutes;
+		document.querySelector('.inner-sec').innerText = seconds;
+
+		if (distance < 0) {
+			clearInterval(startTimer);
+			document.getElementById('demo').innerHTML = 'EXPIRED';
+		}
+	};
+
+	setInterval(startTimer, 1000, time);
+};
+startCountDown(time);
